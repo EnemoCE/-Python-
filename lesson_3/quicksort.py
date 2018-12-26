74647cc59e29fcfdd695b84a0bd15a80e947fa9b
EnemoCE <goncharov.nikita@mail.ru>
2018-12-26 16:10:06 +0300

    подтер принты

--- lesson_3/quicksort.py
+++ lesson_3/quicksort.py
@@ -6,18 +6,13 @@ def quick_sort(l,fi = 0,la = len(l)-1):
         op = l[int((fi+la)/2)]
         i = fi 
         j = la
-        print(i)
-        print(j)
         while i <= j :
-            print("r")
-            print(j)
             while l[i] < op :
                 i += 1
             while l[j] > op:
                 j -= 1
             if i <= j:
                 if l[i] > l[j]:
-                    print('ah,ep')
                     l[i],l[j] = l[j],l[i]
                 i += 1
                 j -= 1
@@ -27,7 +22,6 @@ def quick_sort(l,fi = 0,la = len(l)-1):
                 qr(l,fi,la)
         def rt_2():
             if fi < j :
-                print('r')
                 la = j
                 qr(l,fi,la)
         rt_1()
